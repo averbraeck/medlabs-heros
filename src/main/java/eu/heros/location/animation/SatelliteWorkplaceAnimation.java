92e01a304c86882e149883146e60689bd448585c
Alexander Verbraeck <a.verbraeck@gmail.com>
2024-02-23 14:13:43 +0100

    Copyright updated to 2024

--- src/main/java/eu/heros/location/animation/SatelliteWorkplaceAnimation.java
+++ src/main/java/eu/heros/location/animation/SatelliteWorkplaceAnimation.java
@@ -9,7 +9,7 @@ import nl.tudelft.simulation.medlabs.location.animation.LocationAnimationTemplat
 /**
  * SatelliteWorkplaceAnimation.java.
  * <p>
- * Copyright (c) 2022-2022 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
+ * Copyright (c) 2022-2024 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://opentrafficsim.org/docs/current/license.html">OpenTrafficSim License</a>.
  * </p>
  * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
